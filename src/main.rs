342a686a61ed311790c0d7209e2df426f1e60663
HosokawaR <thin.river.jp@gmail.com>
2021-12-20 02:58:08 +0900

    表示を変更
    コンソールの横幅と縦幅にワールドの広さ合わせるように
    
    色をみやすく

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
+extern crate termsize;
 use rand::Rng;
 use std::{thread, time::Duration};
 
-const WORLD_WIDTH: usize = 20;
-const WORLD_HEIGHT: usize = 10;
+type World = Vec<Vec<u32>>;
 
-type World = [[u32; WORLD_WIDTH]; WORLD_HEIGHT];
-
-fn init_world() -> World {
-    let mut world: World = [[0; WORLD_WIDTH]; WORLD_HEIGHT];
+fn init_world(width: &usize, height: &usize) -> World {
+    let mut world: World = Vec::<Vec<u32>>::with_capacity(*height);
 
     let mut rng = rand::thread_rng();
 
-    for row in 0..WORLD_HEIGHT {
-        for col in 0..WORLD_WIDTH {
-            world[row][col] = rng.gen_range(0..2)
+    for row in 0..*height {
+        world.push(Vec::with_capacity(*width));
+        for _ in 0..*width {
+            world[row].push(rng.gen_range(0..2))
         }
     }
 
     world
 }
 
-fn count_around_living(world: &World, row: usize, col: usize) -> u32 {
+fn count_around_living(
+    world: &World,
+    row: usize,
+    col: usize,
+    width: &usize,
+    height: &usize,
+) -> u32 {
     let mut total_living = 0;
 
     for row_diff in 0..3 {
@@ -30,7 +35,7 @@ fn count_around_living(world: &World, row: usize, col: usize) -> u32 {
             if row_diff == 1 && col_diff == 1 {
                 continue;
             }
-            if r >= 0 && c >= 0 && r < WORLD_HEIGHT as i32 && c < WORLD_WIDTH as i32 {
+            if r >= 0 && c >= 0 && r < *height as i32 && c < *width as i32 {
                 total_living += world[r as usize][c as usize];
             };
         }
@@ -39,25 +44,26 @@ fn count_around_living(world: &World, row: usize, col: usize) -> u32 {
     total_living
 }
 
-fn copy_world(world: &World) -> World {
-    let mut dest_world: World = [[0; WORLD_WIDTH]; WORLD_HEIGHT];
+fn copy_world(world: &World, width: &usize, height: &usize) -> World {
+    let mut dest_world: World = Vec::<Vec<u32>>::with_capacity(*height);
 
-    for row in 0..WORLD_HEIGHT {
-        for col in 0..WORLD_WIDTH {
-            dest_world[row][col] = world[row][col]
+    for row in 0..*height {
+        dest_world.push(Vec::with_capacity(*width));
+        for col in 0..*width {
+            dest_world[row].push(world[row][col])
         }
     }
 
     dest_world
 }
 
-fn advance_world(world: &mut World) {
-    let world_copy = copy_world(world);
+fn advance_world(world: &mut World, width: &usize, height: &usize) {
+    let world_copy = copy_world(world, width, height);
 
-    for row in 0..WORLD_HEIGHT {
-        for col in 0..WORLD_WIDTH {
+    for row in 0..*height {
+        for col in 0..*width {
             let target = world_copy[row][col];
-            let living_number = count_around_living(&world_copy, row, col);
+            let living_number = count_around_living(&world_copy, row, col, width, height);
             if target > 0 {
                 world[row][col] = if 2 <= living_number && living_number <= 3 {
                     1
@@ -71,30 +77,42 @@ fn advance_world(world: &mut World) {
     }
 }
 
-fn display_world(world: &World, generation: u32) {
+fn display_world(world: &World, generation: u32, width: &usize, height: &usize) {
     println!("generation: {}", generation);
-    for row in 0..WORLD_HEIGHT {
-        for col in 0..WORLD_WIDTH {
-            print!("{} ", if world[row][col] > 0 { "■" } else { "□" });
+    for row in 0..*height {
+        for col in 0..*width {
+            print!(
+                "{} ",
+                if world[row][col] > 0 {
+                    "\x1b[32m■\x1b[0m"
+                } else {
+                    "\x1b[30m□\x1b[0m"
+                }
+            );
         }
         println!("")
     }
 }
 
-fn clear() {
-    print!("\x1b[{}A", WORLD_HEIGHT + 1);
+fn clear(height: &usize) {
+    print!("\x1b[{}A", height + 1);
     print!("\x1b[0J");
 }
 
 fn main() {
-    let mut generation = 0;
-    let mut world = init_world();
+    let termsize::Size { rows, cols } = termsize::get().unwrap();
+    let width = cols as usize / 2;
+    let height = rows as usize - 2;
 
-    for _ in 0..100 {
-        display_world(&world, generation);
-        advance_world(&mut world);
-        thread::sleep(Duration::from_secs(1));
-        clear();
+    let mut generation = 0;
+    let mut world = init_world(&width, &height);
+
+    for _ in 0..10_0000 {
+        display_world(&world, generation, &width, &height);
+        advance_world(&mut world, &width, &height);
+        thread::sleep(Duration::from_millis(500));
+        clear(&height);
         generation += 1;
     }
+    display_world(&world, generation, &width, &height);
 }
